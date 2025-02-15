15dfc725e57d48eb6abbcb8bbbe370033a2d513a
Éderson Fernandes <efernandes.tech@gmail.com>
2025-02-15 18:06:26 -0300

    Bug fix

--- android/src/main/java/com/zebrarfidbarcode/rfid/RFIDReaderInterface.java
+++ android/src/main/java/com/zebrarfidbarcode/rfid/RFIDReaderInterface.java
@@ -20,6 +20,7 @@ import com.zebra.rfid.api3.START_TRIGGER_TYPE;
 import com.zebra.rfid.api3.STATUS_EVENT_TYPE;
 import com.zebra.rfid.api3.STOP_TRIGGER_TYPE;
 import com.zebra.rfid.api3.TagData;
+import com.zebra.rfid.api3.TriggerInfo;
 
 import java.util.ArrayList;
 
